fa5c6c15b062da2de562ef8dfe28149207e2341a
Ricardo Hernandez <hs.ricardoj@gmail.com>
2020-10-12 10:06:31 -0400

    added logos to contact page

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -9,14 +9,14 @@ class Contact extends Component {
                 <div className="text-center m-5">
                     <h2>Contact me</h2><hr className="hr1"/>
                     <div class="logos">
-                        <a href="https://linkedin.com/in/ricardo-hernandez-8242b474">
+                        <a target="_blank" href="https://linkedin.com/in/ricardo-hernandez-8242b474">
                             <img src={require('../../images/LI.png')} width="35" height="30" alt="logo"/>
                         </a>
-                        <a href="https://github.com/ricardoh19">
+                        <a target="_blank" href="https://github.com/ricardoh19">
                             <img src={require('../../images/GitHub.png')} width="30" height="30" alt="logo"/>
                         </a>
-                        <a href="https://instagram.com/ricky_codes/">
-                            <img src={require('../../images/IG.png')} width="30" height="30" alt="logo"/>
+                        <a target="_blank" href="https://instagram.com/ricky_codes/">
+                            <img src={require('../../images/IG.png')}  width="30" height="30" alt="logo"/>
                         </a>
                     </div>
                 </div>
