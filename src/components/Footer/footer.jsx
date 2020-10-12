a960fe5dea2f5036b18dc626af1222de793deaeb
Ricardo Hernandez <hs.ricardoj@gmail.com>
2020-10-12 09:20:55 -0400

    Added footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import "./footer.css"
 
 class Footer extends Component {
     state = {  }
     render() { 
         return ( 
-            <footer>
-                <h2>Hello</h2>
-            </footer>
+                <footer className="footer"><hr className="ml-5 mr-5"/>
+                    <div className="container">
+                        <p style={{fontSize: '16px'}}>Copyright @ Ricardo Hernandez 2020</p>
+                    </div>
+                </footer>
          );
     }
 }
