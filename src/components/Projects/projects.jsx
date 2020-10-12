ed7b07119093d19e85cba4beaca4dd92c1996c5b
Ricardo Hernandez <hs.ricardoj@gmail.com>
2020-10-12 10:32:53 -0400

    Added project

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -7,14 +7,19 @@ class Projects extends Component {
             <div className="container">
                 <div className="text-center m-5">
                         <h2>Recent Projects</h2><hr className="ml-5 mr-5"/>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6 offset-md-3">
                             <div className="project1">
+                                <img src={require('../../images/todolist.jpg')} width="470" height="290" alt="logo"/>
+                                    <p>The user can add or delete a task. The task shows date that it was added.</p>
+                                    <h2 className="m-3">To-do List</h2>
+                                    <a class="github" target="_blank" href="https://github.com/ricardoh19/react-To-do-list">
+                                        <img src={require('../../images/GitHub.png')} width="30" height="30" alt="logo"/>
+                                    </a>
+                                <a target="_blank" href="https://wizardly-noyce-328b84.netlify.app/" class="demo">Live Demo</a>
                             </div>
+                            
                         </div>
                     </div>
-            </div>
+
          );
     }
 }
