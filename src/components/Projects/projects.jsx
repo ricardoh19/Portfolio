e398852c22e545e71b5a64c81261033345fa2d11
Ricardo Hernandez <hs.ricardoj@gmail.com>
2020-10-12 11:00:58 -0400

    Added transition CSS to Project image

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -8,7 +8,9 @@ class Projects extends Component {
                 <div className="text-center m-5">
                         <h2>Recent Projects</h2><hr className="ml-5 mr-5"/>
                             <div className="project1">
-                                <img src={require('../../images/todolist.jpg')} width="470" height="290" alt="logo"/>
+                                <a class="project-image" target="_blank" href="https://wizardly-noyce-328b84.netlify.app/">
+                                    <img src={require('../../images/todolist.jpg')} width="470" height="250" alt="logo"/>
+                                </a>
                                     <p>The user can add or delete a task. The task shows date that it was added.</p>
                                     <h2 className="m-3">To-do List</h2>
                                     <a class="github" target="_blank" href="https://github.com/ricardoh19/react-To-do-list">
